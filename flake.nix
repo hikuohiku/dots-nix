2e878dc8cc88f596ccdcdaf1a85de532d164f622
hikuohiku <hikuohiku@gmail.com>
2024-09-15 02:14:17 +0900

    initial commit

---
+++ flake.nix
@@ -0,0 +1,81 @@
+{
+  #ref: https://github.com/yasunori0418/dotfiles/blob/485eee2794c2e5217823b7bba5201e9f9fe16d1e/flake.nix#L2
+  description = "My dotfiles, all my effort, my sword.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    catppuccin.url = "github:catppuccin/nix";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-hardware,
+      home-manager,
+      treefmt-nix,
+      catppuccin,
+    }@inputs:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+    in
+    # code = _: s: s;
+    {
+      # formatter.x86_64-linux = inputs.nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+      formatter.x86_64-linux = treefmtEval.config.build.wrapper;
+
+      nixosConfigurations = {
+        hikuo-desktop = inputs.nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+            inputs.catppuccin.nixosModules.catppuccin
+          ];
+          specialArgs = {
+            inherit inputs;
+          };
+          # inputs.catppuccin.enable = true;
+        };
+      };
+      homeConfigurations = {
+        Home = inputs.home-manager.lib.homeManagerConfiguration {
+          pkgs = import inputs.nixpkgs {
+            system = "x86_64-linux";
+            config.allowUnfree = true;
+          };
+          extraSpecialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            ./home.nix
+          ];
+        };
+      };
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          lua-language-server
+          stylua
+          nil
+        ];
+
+        inputsFrom = with pkgs; [
+          hello
+          gnutar
+        ];
+
+        shellHook = ''
+          exec fish
+        '';
+      };
+    };
+}
