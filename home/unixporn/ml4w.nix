a200662d82946cd45b28142ea655cbd1027c7e48
hikuohiku <hikuohiku@gmail.com>
2024-09-16 01:51:28 +0900

    alacritty

--- home/unixporn/ml4w.nix
+++ home/unixporn/ml4w.nix
@@ -9,11 +9,31 @@
 }:
 {
   home.packages = with pkgs; [
-    (pkgsFromNiqs.bibata-hyprcursor.override
-    {
+    # hypr
+    (pkgsFromNiqs.bibata-hyprcursor.override {
       baseColor = "#FF8300";
       outlineColor = "#FFFFFF";
       watchBackgroundColor = "#001524";
     })
+
   ];
+
+  programs = {
+    alacritty = {
+      enable = true;
+      settings = {
+        font = {
+          size = 12.0;
+          normal.family = "monospace";
+        };
+        window = {
+          opacity = 0.7;
+          padding = {
+            x = 15;
+            y = 15;
+          };
+        };
+      };
+    };
+  };
 }
