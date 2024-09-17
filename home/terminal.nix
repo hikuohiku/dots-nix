deac6b6b43ec473db1083ddcaea4c990ff0e8aa1
hikuohiku <hikuohiku@gmail.com>
2024-09-18 02:36:18 +0900

    terminal

--- home/terminal.nix
+++ home/terminal.nix
@@ -12,6 +12,11 @@
         opacity = 0.7;
       };
       font.size = 12;
+      cursor = {
+        style.shape = "Beam";
+        style.blinking = "On";
+        vi_mode_style.shape = "Block";
+      };
     };
   };
 }
