43d2a8b8fee55fb710a96e4e94493d715627fa0e
hikuohiku <hikuohiku@gmail.com>
2024-09-18 23:26:41 +0900

    user description

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -155,7 +155,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${userInfo.username} = {
     isNormalUser = true;
-    description = "main user";
+    description = userInfo.username;
     extraGroups = [
       "networkmanager"
       "wheel"
