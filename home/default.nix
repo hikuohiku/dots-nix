78a1e6aeec9fda774127e45a6201ed6503a762b4
hikuohiku <hikuohiku@gmail.com>
2024-10-18 03:59:28 +0900

    rm comment

--- home/default.nix
+++ home/default.nix
@@ -120,20 +120,6 @@ rec {
     # flatpak https://wiki.nixos.org/wiki/Flatpak
     flatpak
     gnome-software
-    # ================================== 
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   programs = {
