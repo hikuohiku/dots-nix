7fc3f12904115d52ca16c634c80f624031bbaa01
hikuohiku <hikuohiku@gmail.com>
2024-12-01 22:47:54 +0900

    rm nix-index

--- home/default.nix
+++ home/default.nix
@@ -32,11 +32,6 @@ rec {
 
   xdg.mimeApps.enable = true;
 
-  programs.nix-index = {
-    enable = true;
-    enableFishIntegration = true;
-  };
-
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
