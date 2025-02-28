b066766ff71bc6031632159e0fcc1e92899e7c02
hikuohiku <hikuohiku@gmail.com>
2025-02-28 10:05:53 +0900

    fix core

--- home/core/default.nix
+++ home/core/default.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }:
 {
+  imports = [
+    ./linux.nix
+  ];
+
+  core-linux.enable = pkgs.stdenv.isLinux;
+
   home.packages = with pkgs; [
     # ========== Language Environment ==========
     gcc
