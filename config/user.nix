71bd15a450ddd5f724579b8e73a259b4c002a193
hikuohiku <hikuohiku@gmail.com>
2025-03-01 14:39:59 +0900

    add home path

--- config/user.nix
+++ config/user.nix
@@ -2,6 +2,7 @@
   username = "hikuo";
   hostname = "hikuo-macbook";
   system = "aarch64-darwin";
+  home = /Users/hikuo;
   wallpaperPath = "/home/hikuo/Pictures/wallpaper.jpg";
   git = {
     username = "hikuohiku";
