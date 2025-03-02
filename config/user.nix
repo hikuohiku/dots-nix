3a49294b6e3dab9cee2b51005fb2da191be71726
hikuohiku <hikuohiku@gmail.com>
2025-03-02 11:19:52 +0900

    mainでlinuxがビルドできた方がいいので修正

--- config/user.nix
+++ config/user.nix
@@ -1,8 +1,8 @@
 {
   username = "hikuo";
-  hostname = "hikuo-macbook";
-  system = "aarch64-darwin";
-  home = /Users/hikuo;
+  hostname = "hikuo-desktop";
+  system = "x86_64-linux";
+  home = /home/hikuo;
   wallpaperPath = "/home/hikuo/Pictures/wallpaper.jpg";
   git = {
     username = "hikuohiku";
