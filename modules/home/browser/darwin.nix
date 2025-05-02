ee8b59267c73b77f486a259b496552df95da446f
hikuohiku <hikuohiku@gmail.com>
2025-05-02 20:15:06 +0900

    remove-natsumi

--- modules/home/browser/darwin.nix
+++ modules/home/browser/darwin.nix
@@ -10,14 +10,6 @@
   };
 
   config = lib.mkIf config.browser-darwin.enable {
-    home.file."Library/Application Support/zen/Profiles/hikuo/chrome" =
-      let
-        symlink = config.lib.file.mkOutOfStoreSymlink;
-      in
-      {
-        recursive = true;
-        source = (symlink config.home.homeDirectory + /.ghq/github.com/greeeen-dev/natsumi-browser);
-      };
   };
 
 }
