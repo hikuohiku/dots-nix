d44ff8f44c9b3df22ec5133598ea019611d0d4cc
hikuohiku <hikuohiku@gmail.com>
2025-05-05 01:28:19 +0900

    git logの視認性を良くする

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -44,6 +44,8 @@
           colorArg = "always";
           pager = "delta --paging=never --line-numbers --hyperlinks --hyperlinks-file-link-format='lazygit-edit://{path}:{line}'";
         };
+        allBranchesLogCmd = "git log --graph --color=always --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(#a0a0a0 reverse)%h%Creset %C(cyan)%ad%Creset %C(#dd4814)%ae%Creset %C(yellow reverse)%d%Creset %n%C(white bold)%s%Creset%n'";
+        branchLogCmd = "git log --graph --color=always --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(#a0a0a0 reverse)%h%Creset %C(cyan)%ad%Creset %C(#dd4814)%ae%Creset %C(yellow reverse)%d%Creset %n%C(white bold)%s%Creset%n' {{branchName}}";
       };
       # TODO: git cz
       # customCommands:
