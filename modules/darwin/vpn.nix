680e3881aba4bbd27d828fc682884e462b56ee85
hikuohiku <hikuohiku@gmail.com>
2025-05-05 13:16:19 +0900

    vpnをdaemonにする

--- modules/darwin/vpn.nix
+++ modules/darwin/vpn.nix
@@ -1,6 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  homebrew.brews = [
-    "openfortivpn"
-  ];
+  launchd.daemons."openfortivpn" = {
+    command = "${pkgs.openfortivpn}/bin/openfortivpn -c /Users/hikuo/.vpn/config";
+    serviceConfig = {
+      Disabled = true;
+      StandardErrorPath = "/tmp/openfortivpn.error.log";
+      StandardOutPath = "/tmp/openfortivpn.log";
+    };
+  };
 }
