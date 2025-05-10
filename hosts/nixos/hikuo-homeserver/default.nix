46e69b7ae1bc9cef7d1fabd36a9dc3f49544a612
hikuohiku <hikuohiku@gmail.com>
2025-05-11 04:50:13 +0900

    nix setting

--- hosts/nixos/hikuo-homeserver/default.nix
+++ hosts/nixos/hikuo-homeserver/default.nix
@@ -1,5 +1,15 @@
 { ... }:
 {
+
+  nix = {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+    };
+    # Opinionated: disable channels
+    channel.enable = false;
+  };
+
   users.users.hikuo = {
     isNormalUser = true;
     initialHashedPassword = "$6$y.tuMBFQ5nXEZdI7$wgOsIjdguzJ2TXQ8kyPXkijXM3IM3AOv0eCZ2//RC92xkocsSRWaQ/84qkPBangYWqM82g9VkVxDWw/D3cyeT0";
