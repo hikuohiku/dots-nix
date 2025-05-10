778d320902e6826ba936b89a01480992cc7a67bb
hikuohiku <hikuohiku@gmail.com>
2025-05-11 04:39:10 +0900

    ユーザー設定追加
    初期パスワードは`password`

--- hosts/nixos/hikuo-homeserver/default.nix
+++ hosts/nixos/hikuo-homeserver/default.nix
@@ -1,5 +1,10 @@
 { ... }:
 {
+  users.users.hikuo = {
+    isNormalUser = true;
+    initialHashedPassword = "$6$y.tuMBFQ5nXEZdI7$wgOsIjdguzJ2TXQ8kyPXkijXM3IM3AOv0eCZ2//RC92xkocsSRWaQ/84qkPBangYWqM82g9VkVxDWw/D3cyeT0";
+    extraGroups = [ "wheel" ];
+  };
   # Desktop environments
   services.displayManager.sddm = {
     enable = true;
