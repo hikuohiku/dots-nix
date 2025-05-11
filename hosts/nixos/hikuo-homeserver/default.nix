c75c58da18c2559b806dc8106e197f2f998f25ae
hikuohiku <hikuohiku@gmail.com>
2025-05-11 15:48:42 +0900

    いろいろ
    https://www.joshuamlee.com/nixos-proxmox-vm-images/

--- hosts/nixos/hikuo-homeserver/default.nix
+++ hosts/nixos/hikuo-homeserver/default.nix
@@ -1,10 +1,17 @@
-{ ... }:
+{ modulesPath, lib, ... }:
 {
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
 
   nix = {
     settings = {
       # Enable flakes and new 'nix' command
       experimental-features = "nix-command flakes";
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
     };
     # Opinionated: disable channels
     channel.enable = false;
@@ -15,6 +22,19 @@
     initialHashedPassword = "$6$y.tuMBFQ5nXEZdI7$wgOsIjdguzJ2TXQ8kyPXkijXM3IM3AOv0eCZ2//RC92xkocsSRWaQ/84qkPBangYWqM82g9VkVxDWw/D3cyeT0";
     extraGroups = [ "wheel" ];
   };
+
+  boot.loader.grub.enable = lib.mkDefault true;
+  boot.loader.grub.devices = [ "nodev" ];
+  boot.growPartition = true;
+
+  fileSystems."/" = lib.mkDefault {
+    device = "/dev/disk/by-label/nixos";
+    autoResize = true;
+    fsType = "ext4";
+  };
+
+  networking.hostName = "base";
+  services.qemuGuest.enable = true;
   services.cloud-init.network.enable = true;
   # Desktop environments
   services.displayManager.sddm = {
