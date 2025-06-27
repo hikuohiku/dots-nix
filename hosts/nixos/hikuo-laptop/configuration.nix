16b04ba48673b922b194cda140406959eb9f67ab
Hiroki Ando <hikuohiku@gmail.com>
2025-06-27 22:04:44 +0900

    nix settings

--- hosts/nixos/hikuo-laptop/configuration.nix
+++ hosts/nixos/hikuo-laptop/configuration.nix
@@ -5,11 +5,26 @@
       ./hardware-configuration.nix
     ];
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  # Nix configuration
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
 
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
