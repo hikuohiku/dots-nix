2c04a0db72738edacd4f915d2c7724f3beed11e5
hikuohiku <hikuohiku@gmail.com>
2025-10-22 02:11:31 +0900

    fallbacks

--- modules/home/skhd/default.nix
+++ modules/home/skhd/default.nix
@@ -1,9 +1,22 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  ...
+}:
+let
+  hasYabaiScript = config.my.pkgs ? yabaiScripts.move-to-empty;
+  yabaiMoveToEmptyPathStr =
+    if hasYabaiScript then
+      "${config.my.pkgs.yabaiScripts.move-to-empty}/bin/yabai-move-to-empty"
+    else
+      "yabai-move-to-empty";
+
+in
 {
   xdg.configFile = {
     "skhd/skhdrc" = {
       source = pkgs.replaceVars ./skhdrc {
-        yabaiMoveToEmpty = "${config.my.pkgs.yabaiScripts.move-to-empty}/bin/yabai-move-to-empty";
+        yabaiMoveToEmpty = yabaiMoveToEmptyPathStr;
       };
     };
   };
