0b0b87cb349ad24cf0d57f589408cbcd2181e373
hikuohiku <hikuohiku@gmail.com>
2025-11-08 15:10:15 +0900

    fix

--- modules/home/git/lazygit/darwin.nix
+++ modules/home/git/lazygit/darwin.nix
@@ -6,7 +6,7 @@
 {
   config = lib.mkIf pkgs.stdenv.isDarwin {
     home.file."Library/Application Support/lazygit/config.yml" = {
-      source = ./lazygit-config.yml;
+      source = ./config.yml;
     };
   };
 }
