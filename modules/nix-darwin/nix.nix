9edddd67cba5a729337d2791fa415d2be4f14afa
hikuohiku <hikuohiku@gmail.com>
2025-12-14 16:18:45 +0900

    cachix

--- modules/nix-darwin/nix.nix
+++ modules/nix-darwin/nix.nix
@@ -2,4 +2,15 @@
   nix.extraOptions = ''
     extra-experimental-features = nix-command flakes pipe-operators
   '';
+  # Nix 設定
+  nix.settings = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://hikuohiku.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hikuohiku.cachix.org-1:AZwUw2nnqdfm6k5oLyczGRRHMBEQXz0Fo1HzI+RwApg="
+    ];
+  };
 }
