ec3bb18215bc8b989320405529a597b3a635a19c
hikuohiku <hikuohiku@gmail.com>
2025-12-21 04:04:59 +0900

    refactor

--- modules/home/fortivpn/darwin.nix
+++ modules/home/fortivpn/darwin.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 let
   scriptContent =
     builtins.replaceStrings [ "@openfortivpn@" ] [ "${pkgs.openfortivpn}/bin/openfortivpn" ]
       (builtins.readFile ./hikuo-openfortivpn.sh);
   scriptPkg = pkgs.writeShellScriptBin "hikuo-openfortivpn.sh" scriptContent;
 in
-{
+lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
   imports = [
     ./pac
   ];
